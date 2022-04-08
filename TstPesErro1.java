c4b214ae00a0cab8121c43c6356183dbc5f4af34
Joice Kelly <joicekell2807@gmail.com>
2022-04-08 11:48:23 -0300

    Outra forma correta de se fazer p1.getEnder()

--- TstPesErro1.java
+++ TstPesErro1.java
@@ -4,21 +4,17 @@ public class TstPesErro1{
 
 		Leitura l = new Leitura();
 		Pessoa p1 = new Pessoa();
-
-		Endereco e = new Endereco();
 	
 		p1.setCpf(Integer.parseInt(l.entDados("\nInforme o CPF: ")));
 		p1.setNome(l.entDados("Informe o NOME: "));
 
-		e.setRua(l.entDados("Informe a RUA: "));
-		e.setNum(Integer.parseInt(l.entDados("Informe o NUM da casa: ")));
-
-		p1.setEnder(e);
+		p1.getEnder().setRua(l.entDados("Informe a RUA: "));
+		p1.getEnder().setNum(Integer.parseInt(l.entDados("Informe o NUM da casa: ")));
 
 		System.out.println("\nCPF...: "+p1.getCpf());
 		System.out.println("NOME...: "+p1.getNome());
-		System.out.println("E - RUA...: "+p1.getEnder().getRua());
-		System.out.println("E - NUMERO...: "+p1.getEnder().getNum());
+		System.out.println("Ok - RUA...: "+p1.getEnder().getRua());
+		System.out.println("Ok - NUMERO...: "+p1.getEnder().getNum());
 	}
 
 }
