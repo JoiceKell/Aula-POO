efa0bb34e29865679cb50b12fd0ec042296cda4e
Joice Kelly <joicekell2807@gmail.com>
2022-04-08 11:18:54 -0300

    classe Endereco

--- Pessoa.java
+++ Pessoa.java
@@ -1,7 +1,22 @@
 public class Pessoa{
 
-	private int cpf = 0;				//Variáveis globais
+	private int cpf = 0;		
 	private String nome = "";
+	
+	private Endereco ender = new Endereco();
+
+
+//===========================================
+
+	public Endereco getEnder(){
+		return ender;
+	}
+
+	public void setEnder(Endereco ender){
+		this.ender = ender;
+	}
+
+//===========================================
 
 	public int getCpf(){
 		return cpf;
@@ -12,12 +27,7 @@ public class Pessoa{
 	}
 
 	public void setCpf(int cpf){
-		if(cpf > 0){
-			this.cpf = cpf;
-		} else {
-			System.out.println("\nCPF deve ser positivo!");
-			this.cpf = 1501;
-		}
+		this.cpf = cpf;
 	}
 
 	public void setNome(String nome){
--- TstPes.java
+++ TstPes.java
@@ -8,8 +8,13 @@ public class TstPes{
 		p1.setCpf(Integer.parseInt(l.entDados("\nInforme o CPF: ")));
 		p1.setNome(l.entDados("Informe o NOME: "));
 
+		p1.getEnder().setRua(l.entDados("Informe a RUA: "));
+		p1.getEnder().setNum(Integer.parseInt(l.entDados("Informe o NUM da casa: ")));
+
 		System.out.println("\nCPF...: "+p1.getCpf());
 		System.out.println("NOME...: "+p1.getNome());
+		System.out.println("RUA...: "+p1.getEnder().getRua());
+		System.out.println("NUMERO...: "+p1.getEnder().getNum());
 	}
 
 }
