7e0734c38776a05568117d68b0af6bf82ad72274
Joice Kelly <joicekell2807@gmail.com>
2022-04-08 10:34:00 -0300

    Classe Leitura e uso de classe Wrapper

--- TstPes.java
+++ TstPes.java
@@ -4,12 +4,8 @@ public class TstPes{
 
 		Leitura l = new Leitura();
 		Pessoa p1 = new Pessoa();
-
-		String cpf_S = l.entDados("\nInforme o CPF: ");
-		int cpf_I = Integer.parseInt(cpf_S);
-		
 	
-		p1.setCpf(cpf_I);
+		p1.setCpf(Integer.parseInt(l.entDados("\nInforme o CPF: ")));
 		System.out.println("\nCPF...: "+p1.getCpf());
 	}
 
