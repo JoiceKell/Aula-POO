a0f24aaddb60c3bef278bf2425034d275c74de64
Joice Kelly <joicekell2807@gmail.com>
2022-04-07 23:54:50 -0300

    Classe Pessoa tempo de vida das variáveis

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa{
+	public static void main(String avg[]){
+		int cpf = 0;
+		String nome = "";
+
+		cpf = 10;
+		nome = "Jesus";
+
+		System.out.println("\nCPF...: "+cpf);
+		System.out.println("\nNOME...: "+nome);
+	}
+}
