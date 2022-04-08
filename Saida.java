3e3fb8fb6d308c91064c3bd47d478e0349c7babe
Joice Kelly <joicekell2807@gmail.com>
2022-04-07 23:37:23 -0300

    Saida usando o arg[]

--- Saida.java
+++ Saida.java
@@ -1,12 +1,6 @@
 public class Saida{
 	public static void main(String avg[]){
-		System.out.println("\nMAIN - ANTES da chamada do impDados!");
-		
-		impDados(10);
+		System.out.println("\nValor de ARG[0]: "+  avg[0]);
+	}
 
-		System.out.println("\nMAIN - DEPOIS da chamada do impDados!");
-	}
-	public static void impDados(int x){
-		System.out.println("\n X vale: "+x);
-	}
 }
