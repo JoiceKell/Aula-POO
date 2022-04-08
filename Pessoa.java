c64219f1d8ea4011bf08ce56fd8cc695cc04db9b
Joice Kelly <joicekell2807@gmail.com>
2022-04-08 08:25:36 -0300

    Variáveis Globais

--- Pessoa.java
+++ Pessoa.java
@@ -1,23 +1,23 @@
 public class Pessoa{
-	public static void main(String avg[]){
-		int cpf = 0;
-		String nome = "";
 
-		cpf = entCpf(cpf);
-		nome = entNome(nome);
+	static int cpf = 0;		//Variáveis globais
+	static String nome = "";
+
+	public static void main(String avg[]){
+
+		entCpf();
+		entNome();
 
 		System.out.println("\nCPF...: "+cpf);
 		System.out.println("\nNOME...: "+nome);
 	}
 
-	public static int entCpf(int cpf2){
-		cpf2 = 34;
-		return cpf2;
+	public static void entCpf(){
+		cpf = 78;
 	}
 
-	public static String entNome(String nome2){
-		nome2 = "Adonai";
-		return nome2;
+	public static void entNome(){
+		nome = "Joice Kelly";
 	}
 
 }
