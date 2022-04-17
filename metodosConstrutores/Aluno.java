746cf3b31a53b7b4a560d78bcb507a0d48cdcbfb
Joice Kelly <joicekell2807@gmail.com>
2022-04-17 17:50:07 -0300

    Herança Aluno

---
+++ metodosConstrutores/Aluno.java
@@ -0,0 +1,22 @@
+public class Aluno extends Pessoa{
+
+	private int ra;		
+	private String curso;
+
+
+	public int getRa(){
+		return ra;
+	}
+
+	public String getCurso(){
+		return curso;
+	}
+
+	public void setRa(int ra){
+		this.ra = ra;
+	}
+
+	public void setCurso(String curso){
+		this.curso = curso;
+	}
+}
--- metodosConstrutores/TstHer.java
+++ metodosConstrutores/TstHer.java
@@ -18,6 +18,23 @@ public class TstHer{
 		System.out.println("PROF - NUMERO...: "+prof.getEnder().getNum());
 		System.out.println("PROF - SALARIO...: "+prof.getSal());
 		System.out.println("PROF - TITULO...: "+prof.getTitulo());
+
+		Aluno aluno = new Aluno();
+
+		aluno.setCpf(16); 			//aluno acessa por herança a classe Pessoa
+		aluno.setNome("Joice");		
+		aluno.getEnder().setRua("do Choro");
+		aluno.getEnder().setNum(13);
+
+		aluno.setRa(1300);			//Da própria classe aluno
+		aluno.setCurso("Engenharia de Software");
+
+		System.out.println("\nALUNO - CPF...: "+aluno.getCpf());
+		System.out.println("ALUNO - NOME...: "+aluno.getNome());
+		System.out.println("ALUNO - RUA...: "+aluno.getEnder().getRua());
+		System.out.println("ALUNO - NUMERO...: "+aluno.getEnder().getNum());
+		System.out.println("ALUNO - RA...: "+aluno.getRa());
+		System.out.println("ALUNO - CURSO...: "+aluno.getCurso());
 		
 	}
 
