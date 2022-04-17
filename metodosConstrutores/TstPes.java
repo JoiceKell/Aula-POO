ed6922bcef4b79b5443d27cf39451025ba0b1ed4
Joice Kelly <joicekell2807@gmail.com>
2022-04-17 16:45:55 -0300

    Copia p2=p1, mudança dos valores de p2 muda p1

--- metodosConstrutores/TstPes.java
+++ metodosConstrutores/TstPes.java
@@ -6,7 +6,6 @@ public class TstPes{
 
 		p1.setCpf(1);
 		p1.setNome("Ruah");
-		
 		p1.getEnder().setRua("Rua das Flores");
 		p1.getEnder().setNum(10);
 
@@ -17,11 +16,22 @@ public class TstPes{
 
 		Pessoa p2 = new Pessoa();
 		p2 = p1;
+
+		p1.setCpf(16);
+		p1.setNome("Joice Kelly");
+		p1.getEnder().setRua("do Coracao");
+		p1.getEnder().setNum(25);
 		
 		System.out.println("\n\nCPF - P2...: "+p2.getCpf());
 		System.out.println("NOME - P2...: "+p2.getNome());
 		System.out.println("RUA - P2...: "+p2.getEnder().getRua());
 		System.out.println("NUMERO - P2...: "+p2.getEnder().getNum());
+	
+		System.out.println("\nCPF - P1...: "+p1.getCpf());
+		System.out.println("NOME - P1...: "+p1.getNome());
+		System.out.println("RUA - P1...: "+p1.getEnder().getRua());
+		System.out.println("NUMERO - P1...: "+p1.getEnder().getNum());
+	
 	}
 
 }
